81a38e3fa9d04fe009a2c7c6d83e461dde98c917
iso <ismailkurtlojistik@gmail.com>
2023-05-02 11:54:09 +0300

    footer

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -11,10 +11,10 @@ const Blog = () => {
     getData();
   }, []);
   return (
-    <div className="mx-auto max-w-7xl px-6 lg:px-8 mt-2">
+    <div className="mx-auto max-w-7xl px-6 lg:px-8 mt-2 dark:bg-gray-900">
       {api?.length && (
-        <div className="mx-auto max-w-7xl px-6 lg:px-8 bg-slate-100 dark:bg-gray-900  rounded-lg ">
-          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 border-gray-200 py-10   lg:mx-0 lg:max-w-none lg:grid-cols-3">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8 bg-slate-100   rounded-lg ">
+          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 border-gray-200 py-10 dark:bg-gray-900  lg:mx-0 lg:max-w-none lg:grid-cols-3">
             {api.map((data) => {
               const { id, title, body } = data;
               return (
